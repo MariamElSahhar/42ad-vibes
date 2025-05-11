916f3eee55d0216a616f5a9c438828b76d688095
MariamElSahhar <me1693@nyu.edu>
2025-05-11 15:23:15 +0400

    unused variable

--- src/app/admin/events/page.tsx
+++ src/app/admin/events/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Link from "next/link";
 import { createClient } from "@/utils/supabase/client";
 import EventCards from "@/components/event-cards";
 import { Plus } from "lucide-react";
